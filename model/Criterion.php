fb0622d5df6cf466358eeb25004b563cbb6ff78f
root <root@DESKTOP-23SMA0F.localdomain>
2021-01-06 16:29:12 +0100

    Ajout de la fonction de récupération des données par rapport aux critères généraux getResults() de la classe Criterion.php

--- model/Criterion.php
+++ model/Criterion.php
@@ -18,16 +18,88 @@ class Criterion{
 
     #Interrogation de la base de données
     public function getResults(){
-        /*return $this->corpus;*/
-        self::connexion();
+        /* Exemple de requete qui fonctionne avec les critères principaux (pour le corpus on recherche S dans idTok pour le corpus Scoledit:
+        SELECT * FROM `CM2_Scoledit`
+        WHERE IdTok LIKE "%S%"
+        AND Niv LIKE "CM2"
+        AND Categorie LIKE "NOM"
+        AND StatutErreur LIKE "01-Norm%"
+        AND StatutSegm LIKE "01-Norm%"
+        And Lemme LIKE "fois"
+        */
 
+        #Normaliser les critères pour que la requête soit adaptées au données de la bdd
+        $this->normalizeCriterions();
+
+        #Rédiger la requête
+        $request = 'SELECT * FROM `CM2_Scoledit` WHERE IdTok LIKE "'.$this->corpus.'" AND Niv LIKE "'.$this->level.'" AND Categorie LIKE "'.$this->pos.'" AND StatutErreur LIKE "'.$this->errStatus.'" AND StatutSegm LIKE "'.$this->segmStatus.'" AND Lemme LIKE "'.$this->lemma.'"';
+
+        #Connexion à la base de données
+        $db = self::connexion();
+
+        #Requêter la base
+        $response = $db->query($request);
+
+        #Récupérer les informations de la requête
+        $tab = $response->fetch();
+
+        return $tab;
+    }
+
+    protected function normalizeCriterions(){
+        #Normalisation du corpus : E : Ecriscol, S : Scoledit, R : Resolco, sinon on sélectionne tous les corpus avec %
+        if ($this->corpus == "Scoledit"){
+            $this->corpus = "%S%";
+        } elseif ($this->corpus == "Ecriscol"){
+            $this->corpus = "%E%";
+        } elseif ($this->corpus == "Resolco"){
+            $this->corpus = "%R%";
+        } else {
+            $this->corpus = "%";
+        }
+
+        #Normalisation des catégories grammaticales
+        if ($this->pos == "Adverbe"){
+            $this->pos = "ADV";
+        } elseif ($this->pos == "Nom"){
+            $this->pos = "NOM";
+        } elseif ($this->pos == "Nom Propre"){
+            $this->pos = "NAM";
+        } elseif ($this->pos == "Déterminant") {
+            $this->pos = "DET%";
+        } elseif ($this->pos == "Pronom"){
+            $this->pos = "PRO%";
+        } elseif ($this->pos == "Préposition"){
+            $this->pos = "PRP%";
+        } elseif ($this->pos == "Conjonctions"){ #ATTENTION : Conjonctions de coordination et de subordination sont traitées de la même manière avec Treetagger
+            $this->pos = "KON";
+        } elseif ($this->pos == "Abréviations"){
+            $this->pos = "ABR";
+        } elseif ($this->pos == "Préposition"){
+            $this->pos = "INT";
+        } elseif ($this->pos == "Chiffres"){
+            $this->pos = "NUM";
+        } else {
+            $this->pos = "%";
+        }
+
+        #Normalisation des Statuts d'erreur
+        $this->errStatus = "%".$this->errStatus;
+
+        #Normalisation des Status de segments
+        $this->segmStatus = "%".$this->segmStatus;
+
+        #Normalisation du lemme
+        if($this->lemma == ""){
+            $this->lemma = "%";
+        }
     }
 
     #Connexion à la base de données
-    public static function connexion(){
+    protected static function connexion(){
         try { /* tentative de connexion à la BD*/
-            $bdd = new PDO('mysql:host=localhost;dbname=scoledit', 'scoledit', 'projetpro');
-            return $bdd;
+            $db = new PDO('mysql:host=localhost;dbname=scoledit', 'scoledit', 'projetpro');
+            return $db;
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
--- model/test.php
+++ model/test.php
@@ -2,7 +2,7 @@
 include"Criterion.php";
 include"CriterionAdjective.php";
 
-$criterion = new Criterion("Scoledit", "CM2", "nom","none","base","manger");
-/*var_dump($criterion->getResults());*/
-$criterionAdj = new CriterionAdjective("Scoledit", "CM2", "nom","none","base","manger","femme", "pluriel", "accord", "pass", "non");
+$criterion = new Criterion("Scoledit", "CM2", "Nom","none","base","manger");
+$criterion->getResults();
+$criterionAdj = new CriterionAdjective("Scoledit", "CM2", "NOM","none","base","manger","femme", "pluriel", "accord", "pass", "non");
 /*var_dump($criterionAdj->getResultsAdjective());*/

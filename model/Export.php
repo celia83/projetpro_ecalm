8277fbe7302db2a371b441628c83469950554e1a
root <root@DESKTOP-23SMA0F.localdomain>
2021-02-10 10:57:34 +0100

    Modification de la fonction d'exportation

--- model/Export.php
+++ model/Export.php
@@ -1,6 +1,6 @@
 <?php
 include_once "DataBase.php";
-class Export{
+class Export {
 	protected $word;
     protected $nbLine;
 
@@ -17,33 +17,35 @@ class Export{
 		$i=0;
 		$flag = false;
 		$phrase="";
+		$finalSentencesList ="";
 
-			switch($i) {
-				case ($tabSentences[$i]['SegNorm'] == '<sent>') :
-					$flag = true;
-				break;
-				case ($tabSentences[$i]['SegNorm'] == '</sent>') :
-					$count= $i;
-				break;
-				case ($flag == false) :
-					$i=$i+1;
-			    break;
-				case ($flag == true) :
-                while (($i != $count) && ($i < count($tabSentences))) {
-                    $j = $i-1;
-                    $IdProd = $tabSentences[$i]['IdProd'];
-                    if (($tabSentences[$i]['Lemme'] != '<sent>') && ($tabSentences[$i]['Lemme'] != '</sent>') && ($tabSentences[$i]['Lemme'] != '<FIN>')){
-                        $phrase = $phrase. " " .$tabSentences[$i]['SegTrans'];
-                        //var_dump($tabSentences[$i]);
-                    } else if ($tabSentences[$i]['Categorie'] != 'SENT'){
-                        $phrase = $phrase. " " .$tabSentences[$i]['SegTrans']. "\n";
-                    }
-                    if ($tabSentences[$j]['IdProd'] != $IdProd) {
-                        $phrase = $phrase."\n";
-                    }
+        switch($i) {
+            case ($tabSentences[$i]['SegNorm'] == '<sent>') :
+                $flag = true;
+            break;
+            case ($tabSentences[$i]['SegNorm'] == '</sent>') :
+                $count= $i;
+            break;
+            case ($flag == false) :
                 $i=$i+1;
-				}
-				return $phrase;
-}
-}
+            break;
+            case ($flag == true) :
+            while (($i != $count) && ($i < count($tabSentences))) {
+                $j = $i-1;
+                $IdProd = $tabSentences[$i]['IdProd'];
+                if (($tabSentences[$i]['Lemme'] != '<sent>') && ($tabSentences[$i]['Lemme'] != '</sent>') && ($tabSentences[$i]['Lemme'] != '<FIN>')){
+                    $phrase = $phrase. " " .$tabSentences[$i]['SegTrans'];
+                    //var_dump($tabSentences[$i]);
+                } else if ($tabSentences[$i]['Categorie'] != 'SENT'){
+                    $phrase = $phrase. " " .$tabSentences[$i]['SegTrans']. "\n";
+                }
+                if ($tabSentences[$j]['IdProd'] != $IdProd) {
+                    $phrase = $phrase."\n";
+                }
+                $i=$i+1;
+            }
+                $finalSentencesList = $finalSentencesList . $phrase;
+        }
+        return  $finalSentencesList;
+    }
 }

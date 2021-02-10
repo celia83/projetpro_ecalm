248dbca6014392c54f1586103382326a3ca9eb22
OceaneG <75438061+OceaneG@users.noreply.github.com>
2021-02-10 09:40:25 +0100

    Ajout de l'exportation
    Ajout de l'exportation

--- model/test.php
+++ model/test.php
@@ -2,3 +2,7 @@
 require("User.php");
 $userVerification = new User("scoledit", "projetpro");
 var_dump($userVerification->verifyUser());
+
+require("Export.php");
+$Verification = new Export("le",10);
+var_dump($Verification->sentences());

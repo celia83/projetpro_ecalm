d7822b5b1b5e9808a2b35f929f9cbd421223affa
OceaneG <75438061+OceaneG@users.noreply.github.com>
2021-02-11 00:06:15 +0100

    Ajout Database qui fonctionne pour moi
    Ajout Database qui fonctionne pour moi

---
+++ model/DataBase1.php
@@ -0,0 +1,53 @@
+<?php
+
+class DataBase {
+
+    /**
+     * Permet la connexion à la base de données
+     * @return PDO $db
+     */
+    private static function connection(){
+        try { /* tentative de connexion à la BD*/
+            $db = new PDO('mysql:host=localhost;dbname=scoledit', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            return $db;
+        } catch (Exception $e) {
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+
+    /**
+     * Fonction qui se connecte à une base de données, éxecute la requête passée en paramètre et retourne un tableau des éléments ainsi sélectionnésù
+     * @param string $request
+     * @return array $tab un tableau contenant le résultat de la requête
+     */
+    public function getData($request){
+        #Connexion à la base de données
+        $db = self::connection();
+        #Requêter la base
+        try {
+            $response = $db->query($request);
+
+            #Récupérer les informations de la requête (le mode PDO::FETCH_ASSOC permet d'éviter que le résultats ne dédouble les colonnes)
+            $tab = array();
+            while ($enr = $response->fetch(PDO::FETCH_ASSOC)) {
+                array_push($tab, $enr);
+            }
+        } catch (Exception $e){
+            die ('Erreur : ' . $e->getMessage());
+        }
+        return $tab;
+    }
+
+    public function addOrDelData($request){
+        #Connexion à la base de données
+        $db = self::connection();
+        #Requêter la base
+        try {
+            $db->query($request);
+        } catch (Exception $e){
+            die ('Erreur : ' . $e->getMessage());
+        }
+    }
+}
+
+

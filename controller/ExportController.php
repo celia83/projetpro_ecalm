b0b3935de90d9d10f15bd420a3b97c239f45239a
OceaneG <75438061+OceaneG@users.noreply.github.com>
2021-02-11 10:52:11 +0100

    Modification ExportController
    Modification ExportController

--- controller/ExportController.php
+++ controller/ExportController.php
@@ -1,8 +1,30 @@
 <?php
 
+// Chargement de la classe
 include_once "C:/wamp/www/model/Export.php";
 
+/**
+ * Classe ExportController
+ *
+ * Cette classe contient le contrôleur nécessaire à l'exportation de l'exemplier
+ *
+ * PHP version 5.6
+ *
+ * @author Océane Giroud <oceane.giroud@etu.univ-grenoble-alpes.fr>
+ */
+
 class ExportController{
+	
+	/**
+     * Fonction Export($word, $nbLine)
+     *
+     * Contrôleur pour l'exporation de l'exemplier : Permet de retourner les phrases d'exemple à partir du mot choisi par l'utilisateur au format JSON pour un traitement avec ajax..
+     *
+     * @param $word
+     * @param $nbLine
+     * @return void
+     * @throws Exception
+     */
 
     public function Export($word,$nbLine){
         $exporter = new Export($word,$nbLine);
